eec391ae7bd698ef7819860f4adf25fcdb4521e7
Landenivytech <ltillman16@ivytech.edu>
2025-09-03 10:01:28 -0400

    Create pythonapp assignment.py

---
+++ pythonapp assignment.py
@@ -0,0 +1,7 @@
+GPA = float(input("your GPA: "))
+name = input("your name: ")
+last_name = input("your last name: ")
+if GPA >= 3.5:
+    print(name + ' you have made the Dean List.')
+if GPA >= 3.25:
+    print(name + ' you have made Honor Roll')
